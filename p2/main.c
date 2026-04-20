c8da5bff582ed44ff05983171e3a1741ad402bcc
2024125072 <jaekun1234663@gmail.com>
2026-04-20 15:47:53 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -4,7 +4,6 @@ int main(void)
 {
     int stringSize = 0, alphabetCount = 0, numberCount = 0, i = 0;
     int alphabetCountMax = 0, numberCountMax = 0;
-    int isAlphabetSequence = 0, isNumberSequence = 0;
     char temp = '\0';
     
     scanf("%d", &stringSize);
@@ -12,26 +11,16 @@ int main(void)
     for (i=0; i < stringSize; i++) {
         scanf("%c", &temp);
         if (temp >= 'a' && temp <= 'z') {
-            isAlphabetSequence = 1;
-            isNumberSequence = 0;
-        } else if (temp >= '0' && temp <= '9') {
-            isNumberSequence = 1;
-            isAlphabetSequence = 0;
-        } else {
-            // not lower or number
-            
-        }
-        if (isAlphabetSequence) {
-            isNumberSequence = 0;
             numberCount = 0;
             alphabetCount++;
             if (alphabetCount > alphabetCountMax) alphabetCountMax = alphabetCount;
-        }
-        if (isNumberSequence) {
-            isAlphabetSequence = 0;
+        } else if (temp >= '0' && temp <= '9') {
             alphabetCount = 0;
             numberCount++;
             if (numberCount > numberCountMax) numberCountMax = numberCount;
+        } else {
+            // not lower or number
+            
         }
     }
     
